a620b324eacf3a8739dee0026d941eead39e4e46
SmbdNick <somebodynicolas@gmail.com>
2025-01-29 15:23:47 +0300

    I realy love commiting!

---
+++ ExampleBebra.java
@@ -0,0 +1,8 @@
+class ExampleBebra{
+	public static void main(String[] args){
+		Object a = new String("Hello");
+		
+		System.out.println(a);
+		
+	}
+}
---
+++ GreetingProg.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class GreetingProg {
+	public static void main (String[] args){
+		System.out.println("Hello, World");
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("Enter a number of ways to kill a \nPathfinder party: ");
+		String string = s.nextLine();
+		
+		System.out.print("Pizda Kostyanu " + string + " sposobamy");
+	}
+}
